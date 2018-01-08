5a25f14ef138e426e3f65dfe5c7bc914104e8961
admin <admin@192.168.1.10>
2018-01-08 22:05:55 +1100

    uploading all files

---
+++ src/test/java/manpack/fbTest.java
@@ -0,0 +1,24 @@
+package manpack;
+
+import org.testng.annotations.Test;
+
+public class fbTest {
+
+@Test
+	public void openbrowser()
+	{
+		System.out.println("Browser is opened");
+	}
+	
+	@Test
+	public void navtofacebook()
+	{
+		System.out.println("facebook login page opened");
+	}
+	
+	@Test
+	public void loginfb()
+	{
+		System.out.println("username password and login");
+	}
+}
---
+++ src/test/java/manpack/gmailTest.java
@@ -0,0 +1,24 @@
+package manpack;
+
+import org.testng.annotations.Test;
+
+public class gmailTest {
+
+	@Test
+	public void openbrowser()
+	{
+		System.out.println("Browser is opened");
+	}
+	
+	@Test
+	public void navtogmail()
+	{
+		System.out.println("gmail login page opened");
+	}
+	
+	@Test
+	public void logingmail()
+	{
+		System.out.println("username password and login");
+	}
+}
