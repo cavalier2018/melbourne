0c658edd67327c193d253ae1d7032b2be33107ce
admin <admin@192.168.1.10>
2018-01-08 21:53:01 +1100

    second commit

---
+++ src/test/java/manpack/hotmailTest.java
@@ -0,0 +1,24 @@
+package manpack;
+
+import org.testng.annotations.Test;
+
+public class hotmailTest {
+
+	@Test
+	public void openbrowser()
+	{
+		System.out.println("Browser is opened");
+	}
+	
+	@Test
+	public void navtohotmail()
+	{
+		System.out.println("hotmail login page opened");
+	}
+	
+	@Test
+	public void loginhotmail()
+	{
+		System.out.println("username password and login");
+	}
+}
